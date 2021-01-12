f4df548c71a1613ab32a5df020c945eb34ced0f2
Roman Filippov <rf@romanfilippov.com>
2021-01-12 08:46:58 +0700

    refactor: removes stale code

--- src/types/pino-pretty/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'pino-pretty' {
-  import { PrettyOptions } from 'pino'
-
-  export default function prettyFactory(
-    options: PrettyOptions,
-  ): (inputData: object | string) => string
-}
